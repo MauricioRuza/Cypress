7d2bfc64b38405367314dd1676645a30af12a0e7
Dolitino <mauricruza4@hotmail.com>
2026-05-12 17:35:38 -0300

    creating some new tests

--- cypress/e2e/Rahul Course/spec2.cy.js
+++ cypress/e2e/Rahul Course/spec2.cy.js
@@ -6,27 +6,18 @@ describe("My first Test Suite",()=>{
 
         cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
         cy.get('.search-keyword').type('Ca');
-        cy.wait(2000)
-
+        cy.wait(1000)
         cy.get('.products').as('productLocator')
-
-        cy.get('@productLocator').find('.product').should('have.length',4)
-        cy.get('@productLocator').find('.product').eq(2).contains('ADD TO CART').click()
         cy.get('@productLocator').find('.product').each(($el, index, $list)=>{
             const productName = $el.find('h4.product-name').text()
             if(productName.includes('Cashews')){
                 cy.wrap($el).find('button').click()
             }
         })
-
-        cy.get('.brand').then((logoElement)=>{
-            cy.log(logoElement.text())
-        })
-
-        cy.get('.brand').should('have.text', 'GREENKART')
-
-
-
+        cy.get('.cart-icon > img').click()
+        cy.get('.cart-preview').find('button').click()
+        cy.contains('Place Order').click()
+        cy.get('select')
     })
     
 
---
+++ cypress/e2e/Rahul Course/spec3.cy.js
@@ -0,0 +1,30 @@
+/// <reference types="cypress" />
+
+describe("My third Test Suite",()=>{
+
+    it("First third test Case",()=>{
+
+        cy.visit("https://rahulshettyacademy.com/AutomationPractice/");
+        cy.get('#checkBoxOption2').check().should('be.checked').and('have.value','option2');
+        cy.get('#checkBoxOption2').uncheck().should('not.be.checked');
+        cy.get('input[type="checkbox"]').check(['option3']);
+        cy.get('Select').select('Option2').should('have.value','option2')
+        cy.get('#autocomplete').type('ind')
+        cy.get('.ui-menu-item div').each(($el, index, $list)=>{
+            const item = $el.text();
+            if(item === 'India'){
+                cy.wrap($el).click()
+            }
+        })
+        cy.get('#autocomplete').should('have.value','India')
+        cy.get('#displayed-text').should('be.visible')
+        cy.get('#hide-textbox').click()
+        cy.get('#displayed-text').should('not.be.visible')
+        cy.get('#show-textbox').click()
+        cy.get('#displayed-text').should('be.visible')
+        cy.get('input[type="radio"]').check(['radio2']).should('be.checked')
+
+    })
+    
+
+})

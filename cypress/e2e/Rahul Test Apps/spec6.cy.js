db9120b1b59f10528e8e1c7198ac15845c7f337c
Dolitino <mauricruza4@hotmail.com>
2026-05-15 12:51:44 -0300

    changed the name of the folder

---
+++ cypress/e2e/Rahul Test Apps/spec6.cy.js
@@ -0,0 +1,13 @@
+/// <reference types="cypress" />
+
+describe("My sixth Test Suite",()=>{
+
+    it("First sixth test Case",()=>{
+
+        cy.visit("https://rahulshettyacademy.com/AutomationPractice/");
+        
+
+    })
+    
+
+})
